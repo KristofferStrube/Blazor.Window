5bb07d5a0d972f61727f2d469038765eb8e26124
KristofferStrube <kristoffer.strube@gmail.com>
2024-04-01 17:00:44 +0200

    Updated to include origin in MessageEvent.

--- src/KristofferStrube.Blazor.Window/Events/MessageEvent.cs
+++ src/KristofferStrube.Blazor.Window/Events/MessageEvent.cs
@@ -5,6 +5,10 @@ using Microsoft.JSInterop;
 
 namespace KristofferStrube.Blazor.Window;
 
+/// <summary>
+/// Messages in server-sent events, cross-document messaging, channel messaging, broadcast channels, and WebSockets use the <see cref="MessageEvent"/> interface for their message events.
+/// </summary>
+/// <remarks><see href="https://html.spec.whatwg.org/#messageevent">See the API definition here</see>.</remarks>
 public class MessageEvent : Event, IJSCreatable<MessageEvent>
 {
     /// <summary>
@@ -55,4 +59,14 @@ public class MessageEvent : Event, IJSCreatable<MessageEvent>
     /// </summary>
     /// <returns></returns>
     public ValueReference Data => new(JSRuntime, JSReference, "data");
+
+    /// <summary>
+    /// Gets the origin of the message, for server-sent events and cross-document messaging.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<string> GetOriginAsync()
+    {
+        IJSObjectReference helper = await windowHelperTask.Value;
+        return await helper.InvokeAsync<string>("getAttribute", JSReference, "origin");
+    }
 }
